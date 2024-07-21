e861bd8f3de36c1d6d0b18b2ee8031b9fc04c607
Demostat101 <demostat1995@gmail.com>
2024-07-21 04:10:23 +0100

    modified

--- src/DashBoardContext.jsx
+++ src/DashBoardContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import useLocalStorage from "use-local-storage";
+import useAxiosFetch from "./useAxiosFetch/UseAxiosFetch";
+
 
 export const dashBoardContext = createContext();
 
@@ -8,21 +10,25 @@ export const Context = () => {
 };
 
 export const ContextProvider = ({ children }) => {
+  const { data, fetchError, isLoading } = useAxiosFetch(`http://localhost:4000/data`)
   const [open, setOpen] = useLocalStorage(false);
   const [name, setName] = useState("Esther");
+
+  // const [apiData, setApiData] = useState([]);
   // const [menu, setMenu] = useState(false);
 
   // const handleMenuChange = () => {
   //   setMenu((m) => !m);
   // };
 
+console.log(data);
   const handleOpenBar = () => {
     const close = !open;
     setOpen(close);
   };
 
   return (
-    <dashBoardContext.Provider value={{ handleOpenBar, open, name/* ,menu,handleMenuChange */ }}>
+    <dashBoardContext.Provider value={{ handleOpenBar, open, name , data, fetchError, isLoading/* ,menu,handleMenuChange */ }}>
       {children}
     </dashBoardContext.Provider>
   );
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
+import { Context } from "../DashBoardContext";
+import { useEffect, useState } from "react";
+
 
 const Employee = () => {
-  return <div className="text-red-800">EMPLOYEE</div>;
+  const {data} = Context()
+  const [page, setPage] = useState(0);
+  const [rowPerPage, setRowPerPage] = useState(5);
+
+
+
+  const columns = [
+    {
+      id:"id",
+      headerName:"ID",
+    },
+    {
+      id:"starting_at",
+      headerName:"Date-Time",
+      
+    },
+    {
+      id:"result_info",
+      headerName:"Match-Result",
+    },
+    {
+      id:"name",
+      headerName:"NAME",
+    }
+  ]
+
+  const handlePageChange = (event, newpage) => {
+    setPage(newpage)
+  }
+  const handlePerPageChange = (event) => {
+    setRowPerPage(+event.target.value)
+    setPage(0)
+  }
+
+  
+  return <div className="text-red-800 w-full border-solid border-2 border-green-500 h-[100dvh]">
+    <div className="text-center font-[700] w-full border-solid border-2 border-yellow-500">EMPLOYEE</div>
+
+    <Paper className="w-full h-[100%]">
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              {columns.map((column,index)=>{
+                return <TableCell key={index}>
+                  {column.headerName}
+                </TableCell>
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data && data.slice(page*rowPerPage, page*rowPerPage+rowPerPage).map((values,index)=>{
+              return <TableRow key={index}>
+                  {
+                    columns && columns.map((column,index)=>{
+                      let value = values[column.id];
+                      
+                      return <TableCell key={index}>
+                        {value}
+                        
+                      </TableCell>
+                    })
+                  }
+              </TableRow>
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination rowsPerPageOptions={[5,10,15]} page={page} count={data.length} rowsPerPage={rowPerPage} component="div" onPageChange={handlePageChange} onRowsPerPageChange={handlePerPageChange}>
+            
+      </TablePagination>
+
+    </Paper>
+
+      
+    </div>;
 };
 
 export default Employee;
